679ecd30efdfbae42204243d8bcd08fc7973a007
magilo <53962625+magilo@users.noreply.github.com>
2022-03-05 06:06:58 -0500

    first commit

---
+++ spring-pg-backend/src/main/java/com/mlo/springpgbackend/SpringPgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mlo.springpgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPgBackendApplication.class, args);
+	}
+
+}
---
+++ spring-pg-backend/src/test/java/com/mlo/springpgbackend/SpringPgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlo.springpgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
