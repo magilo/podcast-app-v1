80d636be320e47be8d29a18a61297144a03bbe9f
magilo <53962625+magilo@users.noreply.github.com>
2022-03-06 05:59:38 -0500

    get playlist items component

--- src/main/app/src/components/Playlist.js
+++ src/main/app/src/components/Playlist.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-class Playlist extends Component{
-  constructor(props){
+class Playlist extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      playlist:[]
+      playlist: []
     }
   }
 
-  componentDidMount(){
-
+  async componentDidMount() {
+    try {
+      console.log('inside comp mount')
+      const { data } = await axios.get('/api/podcasts')
+      this.setState({ playlist: data });
+    } catch (err) {
+      console.log(err)
+    }
   }
 
+  render() {
+    const { playlist } = this.state;
+    if (playlist.length > 0) {
+      const listItems = playlist.map((p) =>
+        <div className="podcast" key={p.id}>
+          <span > {p.title} {p.name} </span>
+          <button> x</button>
+        </div>)
+      return (
+        <div>
+          {listItems}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+
+        </div>
+      )
+    }
+  }
 }
+
+export default Playlist;
