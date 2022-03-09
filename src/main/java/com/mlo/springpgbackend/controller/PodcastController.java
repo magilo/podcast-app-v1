eee5f7cac1929c39c0f9ac8f7af64bec6c9f7dad
magilo <53962625+magilo@users.noreply.github.com>
2022-03-08 20:20:31 -0500

    can sort by title and author name

--- src/main/java/com/mlo/springpgbackend/controller/PodcastController.java
+++ src/main/java/com/mlo/springpgbackend/controller/PodcastController.java
@@ -1,11 +1,13 @@
 package com.mlo.springpgbackend.controller;
 
+import java.util.ArrayList;
 // import java.util.ArrayList;
 import java.util.List;
 
 import com.mlo.springpgbackend.model.Podcast;
 import com.mlo.springpgbackend.repository.PodcastRepository;
 
+import org.hibernate.annotations.Sort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000/")
@@ -41,20 +44,31 @@ public class PodcastController {
   }
 
   @GetMapping("/podcasts")
-  public ResponseEntity<List<Podcast>> getAllpodcasts() {
+  public ResponseEntity<List<Podcast>> getAllpodcasts(
+      @RequestParam(required = false) String sort,
+      @RequestParam(required = false) String order) {
     try {
-      List<Podcast> podcasts = podcastRepository.findAll();
+      // List<Podcast> podcasts = podcastRepository.findAll();
+
+      List<Podcast> podcasts = new ArrayList<Podcast>();
+      System.out.println(sort + " " + order);
+      if (sort == null || order == null) {
+        podcasts = podcastRepository.findAll();
+      } else if (sort.equals("title") && order.equals("asc")) {
+        podcasts = podcastRepository.findAllByOrderByTitleAsc();
+        System.out.println("title asc");
+      } else if (sort.equals("title") && order.equals("desc")) {
+        podcasts = podcastRepository.findAllByOrderByTitleDesc();
+      } else if (sort.equals("author") && order.equals("asc")) {
+        podcasts = podcastRepository.findAllByOrderByNameAsc();
+      } else if (sort.equals("author") && order.equals("desc")) {
+        podcasts = podcastRepository.findAllByOrderByNameDesc();
+      }
+
       if (podcasts.isEmpty()) {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
       }
       return new ResponseEntity<>(podcasts, HttpStatus.OK);
-      // List<Podcast> podcasts = new ArrayList<Podcast>();
-      // podcastRepository.findAll().forEach(podcasts::add);
-
-      // if (podcasts.isEmpty()) {
-      // return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-      // }
-      // return new ResponseEntity<>(podcasts, HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/main/java/com/mlo/springpgbackend/repository/PodcastRepository.java
+++ src/main/java/com/mlo/springpgbackend/repository/PodcastRepository.java
@@ -1,5 +1,7 @@
 package com.mlo.springpgbackend.repository;
 
+import java.util.List;
+
 import com.mlo.springpgbackend.model.Podcast;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PodcastRepository extends JpaRepository<Podcast, Long> {
   // refer to sorting tutorial
   // https://www.bezkoder.com/spring-data-sort-multiple-columns/
-}
+  public List<Podcast> findAllByOrderByTitleAsc();
 
+  public List<Podcast> findAllByOrderByTitleDesc();
+
+  public List<Podcast> findAllByOrderByNameAsc();
+
+  public List<Podcast> findAllByOrderByNameDesc();
+
+}
